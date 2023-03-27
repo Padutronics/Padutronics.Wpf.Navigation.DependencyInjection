eb19fc7ffa9ac0421de9aec0dbb0f8af9eea96cf
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-03-27 12:09:55 +0200

    Add root container module

---
+++ Source/Padutronics.Wpf.Navigation.DependencyInjection/Modules/WpfNavigationContainerModule.cs
@@ -0,0 +1,10 @@
+﻿using Padutronics.DependencyInjection;
+
+namespace Padutronics.Wpf.Navigation.DependencyInjection.Modules;
+
+public sealed class WpfNavigationContainerModule : IContainerModule
+{
+    public void Load(IContainerBuilder containerBuilder)
+    {
+    }
+}
